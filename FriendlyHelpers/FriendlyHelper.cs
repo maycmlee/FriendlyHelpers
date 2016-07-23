3c25f9158b61af981d089b85a382f002819c7d53
May Lee (InConsulting Inc.) <v-maylee@microsoft.com>
2016-07-23 13:24:19 -0700

    Add method to register user

---
+++ FriendlyHelpers/FriendlyHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyHelpers
+{
+    /// <summary>
+    /// This is the factory class for registering
+    /// new users.
+    /// </summary>
+    public static class FriendlyHelper
+    {
+    
+        #region Methods
+
+        public static User RegisterUser(string firstName, string lastName, string address, string phone)
+        {
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                Phone = phone
+            };
+            return user;
+        }
+        #endregion  
+
+    }
+}
