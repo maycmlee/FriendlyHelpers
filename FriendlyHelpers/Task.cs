228c0d7acf805919a92904af8609ea5a7a75ee57
May <May>
2016-07-23 07:45:07 -0700

    first commit

---
+++ FriendlyHelpers/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyHelpers
+{
+    class Task
+    {
+        /// <summary>
+        /// This class creates a task that needs to be done.
+        /// </summary>
+        #region Properties
+        public string TaskName { get; set; }
+        public DateTime DateandTime { get; set; }
+        public string Category { get; set; }
+        public bool Completed { get; set; }
+        #endregion
+
+
+    }
+}
