5076f5ab030a0dc61b2db26ab3d96b3fb4b411fe
May Lee (InConsulting Inc.) <v-maylee@microsoft.com>
2016-07-23 14:10:40 -0700

    Add Id to User class

--- FriendlyHelpers/User.cs
+++ FriendlyHelpers/User.cs
@@ -9,6 +9,7 @@ namespace FriendlyHelpers
     public class User
     {
         #region Properties
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
