c691dfa19a25867ab2b7420272724b681db33f2b
May Lee (InConsulting Inc.) <v-maylee@microsoft.com>
2016-07-23 14:29:16 -0700

    Make Id as key

--- FriendlyHelpers/User.cs
+++ FriendlyHelpers/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace FriendlyHelpers
     public class User
     {
         #region Properties
+        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
