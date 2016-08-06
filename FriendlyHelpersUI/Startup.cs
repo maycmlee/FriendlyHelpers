c06f76617538a6d230c6e5fd033ae985466eb97f
v-maylee <v-maylee@MININT-207V48G>
2016-08-06 14:07:25 -0700

    Add UI project

---
+++ FriendlyHelpersUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FriendlyHelpersUI.Startup))]
+namespace FriendlyHelpersUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
