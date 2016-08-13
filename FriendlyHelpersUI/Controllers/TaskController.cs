ecc85d7fe67662f6312d61388b9cae9e15e5fbe6
v-maylee <v-maylee@MININT-207V48G>
2016-08-12 17:27:31 -0700

    User method get all tasks by user email.

--- FriendlyHelpersUI/Controllers/TaskController.cs
+++ FriendlyHelpersUI/Controllers/TaskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FriendlyHelpers;
 
 namespace FriendlyHelpersUI.Controllers
 {
@@ -11,6 +12,8 @@ namespace FriendlyHelpersUI.Controllers
         // GET: Task
         public ActionResult Index()
         {
+
+            FriendlyHelper.GetAllTasksByUserEmail(may@may.com);
             return View();
         }
     }
